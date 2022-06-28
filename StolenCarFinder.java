4c278c7dc032efd420bb45009f2787945c7b6424
KayCewu <70000550+KayCewu@users.noreply.github.com>
2022-06-28 20:30:40 +0200

    Add files via upload

---
+++ StolenCarFinder.java
@@ -0,0 +1,36 @@
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cewuk
+ */
+public class StolenCarFinder {
+JFrame CarFinder=new JFrame();
+JPanel Top=new JPanel(new FlowLayout());
+JPanel Middle=new JPanel(new GridLayout(1,1,10,10));
+JPanel Bottom=new JPanel (new GridLayout());
+JLabel Header=new JLabel("Stolen Vehicle Finder");
+JLabel RegNo=new JLabel("Vehicle plane number");
+
+
+
+
+
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
